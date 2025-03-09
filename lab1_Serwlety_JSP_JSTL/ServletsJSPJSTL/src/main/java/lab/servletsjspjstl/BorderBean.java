40fa3118ac971823d346edd6fb1f639fbd002b87
dawidnowakowski <89041701+dawidnowakowski@users.noreply.github.com>
2025-03-09 01:46:05 +0100

    Lab 1 Serwlety, JSP, JSTL finished

---
+++ lab1_Serwlety_JSP_JSTL/ServletsJSPJSTL/src/main/java/lab/servletsjspjstl/BorderBean.java
@@ -0,0 +1,13 @@
+package lab.servletsjspjstl;
+
+public class BorderBean {
+    String borderVisible;
+
+    public String getBorderVisible() {
+        return borderVisible;
+    }
+
+    public void setBorderVisible(String borderVisible) {
+        this.borderVisible = borderVisible;
+    }
+}
