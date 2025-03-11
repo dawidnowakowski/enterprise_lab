0204a3aea3227037048f4a56adef513207481b87
dawidnowakowski <89041701+dawidnowakowski@users.noreply.github.com>
2025-03-11 11:41:06 +0100

    Lab 6 ASP.NET MVC

---
+++ lab6_ASP_NET_1/MvcNews/MvcNews/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<NewsDbContext>(options =>
+options.UseSqlServer(builder.Configuration
+.GetConnectionString("NewsDbContext")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
